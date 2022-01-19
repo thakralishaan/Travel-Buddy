f5cfd8f5c8d815a36189eb9b97d5a06f086316b6
Ishaan Thakral <thakralishaan@gmail.com>
2022-01-20 01:07:04 +0530

    Initial commit

---
+++ src/api/travelbuddyAPI.js
@@ -0,0 +1,41 @@
+/* eslint-disable consistent-return */
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': "df1057f8b9mshd12214038d81e2ap121719jsnb10d586c3131",
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': "df1057f8b9mshd12214038d81e2ap121719jsnb10d586c3131",
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
