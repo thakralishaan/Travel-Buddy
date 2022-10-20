7c9f04e6267cb9ae26e078c07e1f2f21a1591904
Ishaan Thakral <thakralishaan@gmail.com>
2022-10-20 11:56:45 +0530

    navbar_2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({ onPlaceChanged, onLoad }) => {
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
-        <Typography variant="h5" className={classes.title}>
+        <Typography variant="h5" className={classes.title}>       
           Travel Buddy
         </Typography>
         <Box display="flex">
